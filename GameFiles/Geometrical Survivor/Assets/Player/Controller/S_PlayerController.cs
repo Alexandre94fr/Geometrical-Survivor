377f70a7705da1d855fd6c3674f261003085587e
Alexandre <Alexandre94.fr@gmail.com>
2025-02-19 15:26:42 +0100

    🔄 ▸ Adding player rotation for the Mouse and the Controller + The camera nows follows the player

--- GameFiles/Geometrical Survivor/Assets/Player/Controller/S_PlayerController.cs
+++ GameFiles/Geometrical Survivor/Assets/Player/Controller/S_PlayerController.cs
@@ -4,11 +4,15 @@ using UnityEngine;
 public class S_PlayerController : MonoBehaviour
 {
     public static Action<Vector2> _OnPlayerMoveInputEvent;
+    public static Action<Vector2> _OnPlayerRotateEvent;
 
+    [Header(" External references :")]
+    [SerializeField] Transform _playerCameraTransform;
     [SerializeField] Transform _playerTransform;
     //[SerializeField] S_PlayerProperties _playerProperties;
 
     // TEMP: Will be replaced by _playerProperties
+    [Header(" TEMP :")]
     [SerializeField] float _movementSpeed = 5.0f;
     
     Vector2 _movementDirection;
@@ -16,10 +20,12 @@ public class S_PlayerController : MonoBehaviour
     void Start()
     {
         if (!S_VariablesChecker.AreVariablesCorrectlySetted(gameObject.name, null,
-            (_playerTransform, nameof(_playerTransform))
+            (_playerTransform, nameof(_playerTransform)),
+            (_playerCameraTransform, nameof(_playerCameraTransform))
         )) return;
 
         _OnPlayerMoveInputEvent += UpdateMovementDirection;
+        _OnPlayerRotateEvent += UpdatePlayerOrientation;
     }
 
     void Update()
@@ -27,10 +33,18 @@ public class S_PlayerController : MonoBehaviour
         Vector3 positionOffset = _movementSpeed * Time.deltaTime * _movementDirection;
 
         _playerTransform.position += positionOffset;
+        _playerCameraTransform.position = new(_playerTransform.position.x, _playerTransform.position.y, -10);
     }
 
     void UpdateMovementDirection(Vector2 p_newDirection)
     {
         _movementDirection = p_newDirection;
+    }
+
+    void UpdatePlayerOrientation(Vector2 p_newOrientation)
+    {
+        float rotationAngle = Mathf.Atan2(p_newOrientation.y, p_newOrientation.x) * Mathf.Rad2Deg;
+
+        _playerTransform.rotation = Quaternion.Euler(0, 0, rotationAngle - 90);
     }
 }
