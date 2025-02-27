a3751a10888b9f92dffbfbc149bf411c7046632e
Alexandre <Alexandre94.fr@gmail.com>
2025-02-27 17:05:20 +0100

    🔪 ▸ Adding Enemy's attack system

--- GameFiles/Geometrical Survivor/Assets/Enemy/S_Enemy.cs
+++ GameFiles/Geometrical Survivor/Assets/Enemy/S_Enemy.cs
@@ -11,6 +11,9 @@ public class S_Enemy : MonoBehaviour
 
     SpriteRenderer _spriteRenderer;
 
+    bool _canAttack = true;
+    bool _isCollidingPlayer;
+
     void Start()
     {
         if (!S_VariablesChecker.AreVariablesCorrectlySetted(gameObject.name, null,
@@ -21,6 +24,57 @@ public class S_Enemy : MonoBehaviour
         S_EnemyAttributes._OnHealthPointsUpdateEvent += OnHealthUpdate;
 
         _spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    void OnCollisionEnter2D(Collision2D p_collision2D)
+    {
+        if (p_collision2D.transform.CompareTag("Player"))
+        {
+            _isCollidingPlayer = true;
+
+            if (!_canAttack)
+                return;
+            
+            StartCoroutine(StartAttackLoop(
+                _EnemyStatistics._EnemyProperties._AttackCooldownTime,
+                p_collision2D.gameObject.GetComponentInChildren<S_PlayerAttributes>(),
+                _EnemyStatistics._EnemyProperties._AttackDamage
+            ));
+        }
+    }
+
+    void OnCollisionExit2D(Collision2D p_collision2D)
+    {
+        _isCollidingPlayer = false;
+    }
+
+    IEnumerator StartAttackLoop(float p_attackCooldownTime, S_PlayerAttributes p_playerAttributes, int p_attackDamage)
+    {
+        while (_isCollidingPlayer)
+        {
+            if (_canAttack)
+            {
+                StartCoroutine(LaunchAttackCooldown(_EnemyStatistics._EnemyProperties._AttackCooldownTime));
+
+                Attack(p_playerAttributes, p_attackDamage);
+            }
+                
+            yield return null;
+        }
+    }
+
+    IEnumerator LaunchAttackCooldown(float p_attackCooldownTime)
+    {
+        _canAttack = false;
+
+        yield return new WaitForSeconds(p_attackCooldownTime);
+
+        _canAttack = true;
+    }
+
+    void Attack(S_PlayerAttributes p_playerAttributes, int p_attackDamage)
+    {
+        p_playerAttributes._HealthPoints -= p_attackDamage;
     }
 
     void UpdateSprite(S_Enemy p_enemy, Sprite p_newSprite)
